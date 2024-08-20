040b1dbb96676b9308554c933ff7097ca1799065
Avinash <avinashchowdhury81@gmail.com>
2024-08-20 15:45:24 +0530

    connection url

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.status(200).json({
+    msg: "hello World",
+  });
 });
 
 app.listen(6660, () => {
